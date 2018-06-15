a7b28f576126be861d086255399ffc35413e864c
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-06-14 21:17:04 -0400

    Comment-out RHF, include Löwdin orbitals.

--- exe/fci_main.cpp
+++ exe/fci_main.cpp
@@ -22,17 +22,24 @@ int main (int argc, char** argv) {
     // Actual calculations
     // Prepare the AO basis
     libwint::Molecule molecule (xyz_filename);
+    double internuclear_repulsion_energy = molecule.calculateInternuclearRepulsionEnergy();
+
     libwint::AOBasis ao_basis (molecule, basisset);
     ao_basis.calculateIntegrals();
 
-    // Prepare the SO basis from RHF coefficients
-    hf::rhf::RHF rhf (molecule, ao_basis, 1.0e-03);
-    rhf.solve(hf::rhf::solver::SCFSolverType::DIIS);
-    libwint::SOBasis so_basis (ao_basis, rhf.get_C_canonical());
 
-    // Do a Davidson FCI calculation
-    ci::FCI fci_davidson (so_basis, molecule.get_N()/2, molecule.get_N()/2);  // assume N_alpha = N_beta
-    fci_davidson.solve(numopt::eigenproblem::SolverType::DAVIDSON);
+    // Prepare an SOBasis by using Löwding orthogonalization
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> saes (ao_basis.get_S());
+    libwint::SOBasis so_basis (ao_basis, saes.operatorInverseSqrt());  // Löwdin orthogonalization of the AOBasis
+
+//    // Prepare the SO basis from RHF coefficients
+//    hf::rhf::RHF rhf (molecule, ao_basis, 1.0e-03);
+//    rhf.solve(hf::rhf::solver::SCFSolverType::DIIS);
+//    libwint::SOBasis so_basis (ao_basis, rhf.get_C_canonical());
+
+    // Do a FCI calculation
+    ci::FCI fci (so_basis, molecule.get_N()/2, molecule.get_N()/2);  // assume N_alpha = N_beta
+    fci.solve(numopt::eigenproblem::SolverType::DENSE);
 
 
     // Print the energy to an output file
@@ -43,7 +50,7 @@ int main (int argc, char** argv) {
     std::ofstream output_file;
     output_file.open(output_filename, std::fstream::out);
 
-    output_file << std::setprecision(15) << fci_davidson.get_eigenvalue() << std::endl;
+    output_file << std::setprecision(15) << fci.get_eigenvalue() + internuclear_repulsion_energy << std::endl;
 
     output_file.close();
 }
