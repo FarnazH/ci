c624f8f9439a09c449a8f4f9a0abfd317617676a
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-07-05 10:53:32 -0400

    Add tests for ONVExpansion.isEqual(), clean up constructor based on a GAMESS file.

--- include/ONVExpansion.hpp
+++ include/ONVExpansion.hpp
@@ -22,12 +22,16 @@ namespace ci {
 /**
  *  A class that holds a wave function expansion: it is a wrapper around a std::vector<ci::ONVExpansionComponent<T>>,
  *  which holds alpha-ONVs, beta-ONVs and a corresponding coefficient
+ *
+ *  // TODO: should we make this unordered?
+ *  // TODO: should we normalize / require normalization?
  */
 template <typename T>
 class ONVExpansion {
 private:
-    const std::vector<ci::ONVExpansionComponent<T>> expansion;
-    const size_t K;  // number of spatial orbitals
+    std::vector<ci::ONVExpansionComponent<T>> expansion;
+    size_t K;  // number of spatial orbitals
+    size_t dim;  // the dimension of the expansion
 
 
 public:
@@ -38,7 +42,8 @@ public:
      */
     ONVExpansion(const std::initializer_list<ci::ONVExpansionComponent<T>>& list) :
         expansion (list),
-        K (this->expansion[0].alpha.get_K())
+        K (this->expansion[0].alpha.get_K()),
+        dim (this->expansion.size())
     {
         // Check if all the ONVs have the same number of orbitals
         for (const auto& expansion_component : this->expansion) {
@@ -50,79 +55,77 @@ public:
     }
 
 
-//    /**
-//     *  Constructor based on a given @param GAMESS_filename
-//     */
-//    explicit ONVExpansion(const std::string& GAMESS_filename) {
-//
-//        // If the filename isn't properly converted into an input file stream, we assume the user supplied a wrong file
-//        std::ifstream input_file_stream(GAMESS_filename);
-//
-//        if (!input_file_stream.good()) {
-//            throw std::runtime_error("The provided GAMESS file is illegible. Maybe you specified a wrong path?");
-//        }
-//
-//
-//        // Do the actual parsing
-//
-//        // Read in dummy lines up until we actually get to the ONVs and coefficients
-//        std::string line;
-//        size_t K = 0;
-//        while (std::getline(input_file_stream, line)) {
-//            if (line.find("ALPHA") != std::string::npos && line.find("BETA") != std::string::npos &&
-//                line.find("COEFFICIENT") !=
-//                std::string::npos) {  // if find returns an index that's different from the 'not-found' index
-//                std::getline(input_file_stream, line);
-//
-//                // After the 'ALPHA', 'BETA', 'COEFFICIENTS' line, we can actually get the number of orbitals by
-//                // counting the number of dashes (-)
-//                std::vector<std::string> splitted_line;
-//                boost::split(splitted_line, line, boost::is_any_of("|"));
-//
-//                K = splitted_line[0].length();
-//                break;
-//            }
-//        }
-//        if (K == 0) {
-//            throw std::invalid_argument("Couldn't read a number of orbitals from a number of dashes.");
-//        }
-//
-//
-//        // Read in the ONVs and the coefficients by splitting the line on '|', and then trimming
-//        // In the GAMESS format, the bit strings are ordered reverse lexicographically, so we will have to reverse
-//        while (std::getline(input_file_stream, line)) {
-//            std::vector<std::string> splitted_line;  // create a container for the line to be split in
-//            boost::split(splitted_line, line, boost::is_any_of("|"));  // split on '|'
-//
-//
-//            // Create an alpha SpinString for the first field
-//            std::string trimmed_alpha = boost::algorithm::trim_copy(splitted_line[0]);
-//            if (trimmed_alpha.length() != K) {
-//                throw std::invalid_argument(
-//                    "One of the provided alpha ONVs does not have the correct number of orbitals.");
-//            }
-//            std::string reversed_alpha(trimmed_alpha.rbegin(), trimmed_alpha.rend());
-//            bmqc::SpinString<T> alpha(std::stoul(reversed_alpha, nullptr, 2), K);
-//
-//
-//            // Create a beta SpinString for the second field
-//            std::string trimmed_beta = boost::algorithm::trim_copy(splitted_line[1]);
-//            if (trimmed_beta.length() != K) {
-//                throw std::invalid_argument(
-//                    "One of the provided beta ONVs does not have the correct number of orbitals.");
-//            }
-//            std::string reversed_beta(trimmed_beta.rbegin(), trimmed_beta.rend());
-//            bmqc::SpinString<T> beta(std::stoul(reversed_beta, nullptr, 2), K);
-//
-//
-//            // Create a double for the third field
-//            double coefficient = std::stod(splitted_line[2]);
-//
-//
-//            // Add an ONVExpansionComponent to the end of this
-//            this->expansion.emplace_back(ci::ONVExpansionComponent<T>{alpha, beta, coefficient});
-//        }  // while
-//    }
+    /**
+     *  Constructor based on a given @param GAMESS_filename
+     */
+    explicit ONVExpansion(const std::string& GAMESS_filename) {
+
+        // If the filename isn't properly converted into an input file stream, we assume the user supplied a wrong file
+        std::ifstream input_file_stream(GAMESS_filename);
+
+        if (!input_file_stream.good()) {
+            throw std::runtime_error("ONVExpansion(): The provided GAMESS file is illegible. Maybe you specified a wrong path?");
+        }
+
+
+        // Do the actual parsing
+
+        // Read in dummy lines up until we actually get to the ONVs and coefficients
+        std::string line;
+        while (std::getline(input_file_stream, line)) {
+            if (line.find("ALPHA") != std::string::npos
+                && line.find("BETA") != std::string::npos
+                && line.find("COEFFICIENT") != std::string::npos) {  // if find returns an index that's different from the 'not-found' index
+
+                std::getline(input_file_stream, line);  // this line should have dashes
+
+                // We can actually get the number of orbitals by counting the number of dashes
+                std::vector<std::string> splitted_line;
+                boost::split(splitted_line, line, boost::is_any_of("|"));  // split on '|'
+
+                this->K = splitted_line[0].length();
+                break;
+            }
+        }
+
+
+        // Read in the ONVs and the coefficients by splitting the line on '|', and then trimming whitespace
+        // In the GAMESS format, the bit strings are ordered in reverse
+        while (std::getline(input_file_stream, line)) {
+            std::vector<std::string> splitted_line;
+            boost::split(splitted_line, line, boost::is_any_of("|"));  // split on '|'
+
+
+            // Create an alpha SpinString for the first field
+            std::string trimmed_alpha = boost::algorithm::trim_copy(splitted_line[0]);
+            if (trimmed_alpha.length() != this->K) {
+                throw std::invalid_argument("ONVExpansion(): One of the provided alpha ONVs does not have the correct number of orbitals.");
+            }
+            std::string reversed_alpha (trimmed_alpha.rbegin(), trimmed_alpha.rend());
+            // FIXME: depending on the number of orbitals, we should create unsigned long vs dynamic_bitset, so don't use std::stoul per se
+            bmqc::SpinString<T> alpha (std::stoul(reversed_alpha, nullptr, 2), this->K);  // convert a binary string to an unsigned long
+
+
+            // Create a beta SpinString for the second field
+            std::string trimmed_beta = boost::algorithm::trim_copy(splitted_line[1]);
+            if (trimmed_beta.length() != K) {
+                throw std::invalid_argument("ONVExpansion(): One of the provided beta ONVs does not have the correct number of orbitals.");
+            }
+            std::string reversed_beta(trimmed_beta.rbegin(), trimmed_beta.rend());
+            // FIXME: depending on the number of orbitals, we should create unsigned long vs dynamic_bitset, so don't use std::stoul per se
+            bmqc::SpinString<T> beta(std::stoul(reversed_beta, nullptr, 2), K);  // convert a binary string to an unsigned long
+
+
+            // Create a double for the third field
+            double coefficient = std::stod(splitted_line[2]);
+
+
+            // Add an ONVExpansionComponent to the end of this
+            this->expansion.emplace_back(ci::ONVExpansionComponent<T>{alpha, beta, coefficient});
+        }  // while getline
+
+        this->dim = this->expansion.size();
+    }
 //
 //
 //    /**
@@ -192,41 +195,53 @@ public:
 //    }
 
 
-    // OPERATORS
-    ci::ONVExpansionComponent<T> operator[](size_t index) {
+    /*
+     *  OPERATORS
+     */
+    ci::ONVExpansionComponent<T> operator[](size_t index) const {
         return this->expansion[index];
     }
 
-    // PUBLIC METHODS
-//    /**
-//     *  @return if this is equal to @param other, within a given @param tolerance
-//     */
-//    bool isEqual(const ci::ONVExpansion<T>& other, double tolerance=1.0e-12) const {
-//        // Check first on equal dimensions
-//        if (this->size() != other.size()) {
-//            return false;
-//        }
-//
-//
-//        // Check if the SpinStrings are in the same order
-//        // In the mean time construct the eigenvector coefficients, so we don't have to loop over the dimension twice
-//        Eigen::VectorXd this_coefficients = Eigen::VectorXd::Zero(this->size());
-//        Eigen::VectorXd other_coefficients = Eigen::VectorXd::Zero(this->size());
-//        for (size_t i = 0; i < this->size(); i++) {
-//
-//            if (((*this)[i].alpha != other[i].alpha) || ((*this)[i].beta != other[i].beta)) {
-//                return false;
-//            }
-//
-//            this_coefficients(i) = (*this)[i].coefficient;
-//            other_coefficients(i) = other[i].coefficient;
-//        }
-//
-//
-//        // Check if the eigenvectors are equal
-//        // This is the final check, so we can simplify an if-else construct
-//        return cpputil::linalg::areEqualEigenvectors(this_coefficients, other_coefficients, tolerance);
-//    }
+
+    /*
+     *  GETTERS
+     */
+    size_t get_dim() const { return this->dim; }
+
+
+    /*
+     *  PUBLIC METHODS
+     */
+    /**
+     *  @return if this is equal to @param other, within a given @param tolerance
+     */
+    bool isEqual(const ci::ONVExpansion<T>& other, double tolerance=1.0e-12) const {
+
+        // Check first on equal dimensions
+        if (this->dim != other.get_dim()) {
+            return false;
+        }
+
+
+        // Check if the SpinStrings are the same
+        // In the mean time construct the eigenvector coefficients, so we don't have to loop over the dimension twice
+        Eigen::VectorXd this_coefficients = Eigen::VectorXd::Zero(this->dim);
+        Eigen::VectorXd other_coefficients = Eigen::VectorXd::Zero(this->dim);
+        for (size_t i = 0; i < this->dim; i++) {
+
+            if ((this->expansion[i].alpha != other[i].alpha) || (this->expansion[i].beta != other[i].beta)) {
+                return false;
+            }
+
+            this_coefficients(i) = this->expansion[i].coefficient;
+            other_coefficients(i) = other[i].coefficient;
+        }
+
+
+        // Check if the eigenvectors are equal
+        // This is the final check, so we can simplify an if-else construct
+        return cpputil::linalg::areEqualEigenvectors(this_coefficients, other_coefficients, tolerance);
+    }
 };
 
 }  // namespace ci
