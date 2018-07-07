583393214a1a046423a79070ca89c7d865b36b32
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-07-07 09:57:02 -0400

    Now require bmqc v1.2.2.

--- cmake/FindPackages.cmake
+++ cmake/FindPackages.cmake
@@ -14,7 +14,7 @@ find_package(hf 3.1.0 REQUIRED)
 find_package(libwint 3.1.0 REQUIRED)
 
 # Find bmqc for bitset manipulations
-find_package(bmqc 1.0.1 REQUIRED)
+find_package(bmqc 1.2.2 REQUIRED)
 
 # Find numopt
 find_package(numopt 1.2.0 REQUIRED)  # FIXME: this should become 1.3.0
