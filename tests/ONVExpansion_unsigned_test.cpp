ace1aa40a5c4482fb2c0ae022909963e2a858e33
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-07-09 18:52:25 -0400

    Add reference tests from other RDM implementations.

--- tests/ONVExpansion_unsigned_test.cpp
+++ tests/ONVExpansion_unsigned_test.cpp
@@ -432,3 +432,177 @@ BOOST_AUTO_TEST_CASE ( two_rdms_fci_H2O_STO_3G_unsigned ) {
     BOOST_CHECK(cpputil::linalg::areEqual(test_two_rdm_bbbb, ref_two_rdm_bbbb, 1.0e-06));
     BOOST_CHECK(cpputil::linalg::areEqual(test_two_rdm, ref_two_rdm, 1.0e-06));
 }
+
+
+/*
+ *  AUXILIARY FUNCTIONS
+ */
+/**
+ *  @return the trace of the given 2-RDM @param: d(p,p,q,q)
+ *
+ *  In this case, the 'trace' tensor operation should be used, but the current Eigen3 (3.3.4) hasn't released that support yet
+ *  Since Eigen3 hasn't released tensor.trace() yet, we will do the reduction ourselves
+
+ */
+double trace_2RDM(const Eigen::Tensor<double, 4>& d) {
+    // TODO: when Eigen3 releases tensor.trace(), use it to implement the reduction
+
+    auto K = static_cast<size_t>(d.dimension(1));
+
+    double trace = 0.0;
+    for (size_t p = 0; p < K; p++) {
+        for (size_t q = 0; q < K; q++) {
+            trace += d(p,p,q,q);
+        }
+    }
+
+    return trace;
+}
+
+
+double full_trace_2RDM(const Eigen::Tensor<double, 4>& d) {
+    // TODO: when Eigen3 releases tensor.trace(), use it to implement the reduction
+
+    auto K = static_cast<size_t>(d.dimension(1));
+
+    double trace = 0.0;
+    for (size_t p = 0; p < K; p++) {
+        trace += d(p,p,p,p);
+    }
+
+    return trace;
+}
+
+
+Eigen::MatrixXd reduce_2RDM(const Eigen::Tensor<double, 4>& d) {
+    // TODO: when Eigen3 releases tensor.trace(), use it to implement the reduction
+
+    auto K = static_cast<size_t>(d.dimension(1));
+
+    Eigen::MatrixXd D = Eigen::MatrixXd::Zero(K, K);
+    for (size_t p = 0; p < K; p++) {
+        for (size_t q = 0; q < K; q++) {
+            for (size_t r = 0; r < K; r++) {
+                D(p,q) += d(p,q,r,r);
+            }
+        }
+    }
+
+    return D;
+}
+
+
+BOOST_AUTO_TEST_CASE ( cristina_rdms ) {
+
+    // Initialize the matrices/tensors that are read in from test files
+    Eigen::MatrixXd ref_one_rdm_aa = Eigen::MatrixXd::Zero(46,46);
+    Eigen::MatrixXd ref_one_rdm_bb = Eigen::MatrixXd::Zero(46,46);
+    Eigen::MatrixXd ref_one_rdm = Eigen::MatrixXd::Zero(46,46);
+
+
+    Eigen::Tensor<double, 4> ref_two_rdm_aaaa (46,46,46,46);
+    Eigen::Tensor<double, 4> ref_two_rdm_aabb (46,46,46,46);
+    Eigen::Tensor<double, 4> ref_two_rdm_bbaa (46,46,46,46);
+    Eigen::Tensor<double, 4> ref_two_rdm_bbbb (46,46,46,46);
+    Eigen::Tensor<double, 4> ref_two_rdm (46,46,46,46);
+
+    ref_two_rdm_aaaa.setZero();
+    ref_two_rdm_aabb.setZero();
+    ref_two_rdm_bbaa.setZero();
+    ref_two_rdm_bbbb.setZero();
+    ref_two_rdm.setZero();
+
+
+    // Read in the 1- and 2-RDMs (time-consuming step)
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm1s_aa", ref_one_rdm_aa);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm1s_bb", ref_one_rdm_bb);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm1", ref_one_rdm);
+
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm2s_aaaa", ref_two_rdm_aaaa);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm2s_aabb", ref_two_rdm_aabb);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm2s_bbaa", ref_two_rdm_bbaa);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm2s_bbbb", ref_two_rdm_bbbb);
+    cpputil::io::readArrayFromFile("../tests/reference_data/rdm_test/rdm2", ref_two_rdm);
+
+    // The 2-RDMs outputted in the files have different array indices, so we will shuffle them accordingly
+    // Note that .shuffle() returns a copy, and doesn't shuffle in-place
+    Eigen::array<int, 4> shuffle_indices ({0, 2, 1, 3});  // ikjl -> ijkl
+    Eigen::Tensor<double, 4> ref_two_rdm_aaaa_shuffled = ref_two_rdm_aaaa.shuffle(shuffle_indices);
+    Eigen::Tensor<double, 4> ref_two_rdm_aabb_shuffled = ref_two_rdm_aabb.shuffle(shuffle_indices);
+    Eigen::Tensor<double, 4> ref_two_rdm_bbaa_shuffled = ref_two_rdm_bbaa.shuffle(shuffle_indices);
+    Eigen::Tensor<double, 4> ref_two_rdm_bbbb_shuffled = ref_two_rdm_bbbb.shuffle(shuffle_indices);
+    Eigen::Tensor<double, 4> ref_two_rdm_shuffled = ref_two_rdm.shuffle(shuffle_indices);
+
+
+    // Check the traces of the 1-RDMs that are read in
+    BOOST_CHECK(std::abs(ref_one_rdm_aa.trace() - 1) < 1.0e-09);  // 1 alpha electron
+    BOOST_CHECK(std::abs(ref_one_rdm_bb.trace() - 1) < 1.0e-09);  // 1 beta electron
+    BOOST_CHECK(std::abs(ref_one_rdm.trace() - 2) < 1.0e-08);  // 2 electrons
+
+    // Check the traces of the 2-RDMs that are read in
+    BOOST_CHECK(std::abs(trace_2RDM(ref_two_rdm_aaaa_shuffled) - 0) < 1.0e-12);  // 0 pairs of alpha electrons (two_rdm_aaaa is all zeros anyways)
+    BOOST_CHECK(std::abs(trace_2RDM(ref_two_rdm_aabb_shuffled) - 1) < 1.0e-09);  // 1 pair of alpha-beta electrons
+    BOOST_CHECK(std::abs(trace_2RDM(ref_two_rdm_bbaa_shuffled) - 1) < 1.0e-09);  // 1 pair of alpha-beta electrons
+    BOOST_CHECK(std::abs(trace_2RDM(ref_two_rdm_bbbb_shuffled) - 0) < 1.0e-12);  // 0 pairs of beta electrons (two_rdm_bbbb is all zeros anyways)
+    BOOST_CHECK(std::abs(trace_2RDM(ref_two_rdm_shuffled) - 1) < 1.0e-09);  // 1 pair of electrons
+
+    // Check if the read in 1-RDM can be calculated from the read in 2-RDM
+    BOOST_CHECK(ref_one_rdm.isApprox(reduce_2RDM(2*ref_two_rdm_shuffled)));
+
+
+
+    // Read in the reference expansion and calculate its 1- and 2-RDMs
+    ci::ONVExpansion<unsigned long> expansion ("../tests/reference_data/rdm_test/he_asymp0.50_3_gamess");
+    expansion.calculate1RDMs();
+    expansion.calculate2RDMs();
+
+
+    // Check the traces of the calculated 1-RDMs
+    BOOST_CHECK(std::abs(expansion.get_one_rdm_aa().trace() - 1) < 1.0e-09);  // 1 alpha electron
+    BOOST_CHECK(std::abs(expansion.get_one_rdm_bb().trace() - 1) < 1.0e-09);  // 1 beta electron
+    BOOST_CHECK(std::abs(expansion.get_one_rdm().trace() - 2) < 1.0e-08);  // 2 electrons
+
+    // Check the traces of the calculated 2-RDMs
+    BOOST_CHECK(std::abs(trace_2RDM(expansion.get_two_rdm_aaaa()) - 0) < 1.0e-12);  // 0 pairs of alpha electrons (two_rdm_aaaa is all zeros anyways)
+    BOOST_CHECK(std::abs(trace_2RDM(expansion.get_two_rdm_aabb()) - 1) < 1.0e-09);  // 1 pair of alpha-beta electrons
+    BOOST_CHECK(std::abs(trace_2RDM(expansion.get_two_rdm_bbaa()) - 1) < 1.0e-09);  // 1 pair of alpha-beta electrons
+    BOOST_CHECK(std::abs(trace_2RDM(expansion.get_two_rdm_bbbb()) - 0) < 1.0e-12);  // 0 pairs of beta electrons (two_rdm_bbbb is all zeros anyways)
+    BOOST_CHECK(std::abs(trace_2RDM(expansion.get_two_rdm()) - 2) < 1.0e-08);  // 1 pair of electrons times 2
+
+    // Check if the calculated 1-RDM can be calculated from the calculated 2-RDM
+    BOOST_CHECK(expansion.get_one_rdm().isApprox(reduce_2RDM(expansion.get_two_rdm())));
+
+
+
+    // Check if the calculated and read in 1-RDMs are equal
+    BOOST_CHECK(ref_one_rdm_aa.isApprox(expansion.get_one_rdm_aa(), 1.0e-06));
+    BOOST_CHECK(ref_one_rdm_bb.isApprox(expansion.get_one_rdm_bb(), 1.0e-06));
+    BOOST_CHECK(ref_one_rdm.isApprox(expansion.get_one_rdm(), 1.0e-06));
+
+    // Check if the calculated and read in 2-RDMs are equal
+    BOOST_CHECK(cpputil::linalg::areEqual(ref_two_rdm_aaaa_shuffled, expansion.get_two_rdm_aaaa(), 1.0e-12));  // are all zero anyways
+    BOOST_CHECK(cpputil::linalg::areEqual(ref_two_rdm_aabb_shuffled, expansion.get_two_rdm_aabb(), 1.0e-04));
+    BOOST_CHECK(cpputil::linalg::areEqual(ref_two_rdm_bbaa_shuffled, expansion.get_two_rdm_bbaa(), 1.0e-04));
+    BOOST_CHECK(cpputil::linalg::areEqual(ref_two_rdm_bbbb_shuffled, expansion.get_two_rdm_bbbb(), 1.0e-12));  // are all zero anyways
+    BOOST_CHECK(cpputil::linalg::areEqual(2*ref_two_rdm_shuffled, expansion.get_two_rdm(), 1.0e-04));  // factor half difference in other full 2-RDM
+
+
+    // Calculate energies based on contractions of the integrals and density matrices
+    libwint::SOBasis so_basis ("../tests/reference_data/rdm_test/he_asymp0.50_3.FCIDUMP", 46);
+    // Specify the contractions for the relevant contraction of the two-electron integrals and the 2-RDM
+    //      0.5 g(p q r s) d(p q r s)
+    Eigen::array<Eigen::IndexPair<int>, 4> contractions = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1), Eigen::IndexPair<int>(2,2), Eigen::IndexPair<int>(3,3)};
+
+
+    // Calculate the energy based off the read in density matrices
+    double energy_by_contraction_read = (so_basis.get_h_SO() * ref_one_rdm).trace();
+    Eigen::Tensor<double, 0> contraction_read = 0.5 * (so_basis.get_g_SO()).contract(2*ref_two_rdm_shuffled, contractions);
+    energy_by_contraction_read += contraction_read(0);  // as the contraction is a scalar (a tensor of rank 0), we should access by (0)
+    BOOST_CHECK(std::abs(energy_by_contraction_read - -3.0203317600) < 1.0e-08);
+
+    // Calculate the energy based off the calculated density matrices
+    double energy_by_contraction_calculated = (so_basis.get_h_SO() * expansion.get_one_rdm()).trace();
+    Eigen::Tensor<double, 0> contraction_calculated = 0.5 * (so_basis.get_g_SO()).contract(expansion.get_two_rdm(), contractions);
+    energy_by_contraction_calculated += contraction_calculated(0);  // as the contraction is a scalar (a tensor of rank 0), we should access by (0)
+    BOOST_CHECK(std::abs(energy_by_contraction_calculated - -3.0203317600) < 1.0e-09);
+}
